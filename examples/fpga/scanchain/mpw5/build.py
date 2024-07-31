f22af9713282778e752251dc6996a556d4fbf261
Restyled.io <commits@restyled.io>
2024-07-31 21:08:30 +0000

    Restyled by black

--- examples/fpga/scanchain/mpw5/build.py
+++ examples/fpga/scanchain/mpw5/build.py
@@ -19,7 +19,7 @@ chain_width = 1
 ctx = Context()
 gbl_clk = ctx.create_global("clk", is_clock=True)
 gbl_clk.bind((0, 1), 0)
-ctx.create_segment('L1', n_L1, 1)
+ctx.create_segment("L1", n_L1, 1)
 
 # slice
 builder = ctx.build_slice("slice")
@@ -28,41 +28,40 @@ i = builder.create_input("i", 4)
 o = builder.create_output("o", 1)
 lut = builder.instantiate(ctx.primitives["lut" + str(k)], "lut")
 ff = builder.instantiate(ctx.primitives["flipflop"], "ff")
-builder.connect(clk, ff.pins['clk'])
-builder.connect(i, lut.pins['in'])
-builder.connect(lut.pins['out'], o)
-builder.connect(lut.pins['out'], ff.pins['D'], vpr_pack_patterns=('lut_dff', ))
-builder.connect(ff.pins['Q'], o)
+builder.connect(clk, ff.pins["clk"])
+builder.connect(i, lut.pins["in"])
+builder.connect(lut.pins["out"], o)
+builder.connect(lut.pins["out"], ff.pins["D"], vpr_pack_patterns=("lut_dff",))
+builder.connect(ff.pins["Q"], o)
 cluster = builder.commit()
 
 # IOB
 builder = ctx.build_io_block("iob")
 o = builder.create_input("outpad", 1)
 i = builder.create_output("inpad", 1)
-builder.connect(builder.instances['io'].pins['inpad'], i)
-builder.connect(o, builder.instances['io'].pins['outpad'])
+builder.connect(builder.instances["io"].pins["inpad"], i)
+builder.connect(o, builder.instances["io"].pins["outpad"])
 iob = builder.commit()
 
 # CLB
 builder = ctx.build_logic_block("clb")
-clk = builder.create_global(gbl_clk,                        Orientation.south)
-in_ = builder.create_input("in",       N * k // 2,         Orientation.west)
-out = builder.create_output("out",      N,                  Orientation.east)
+clk = builder.create_global(gbl_clk, Orientation.south)
+in_ = builder.create_input("in", N * k // 2, Orientation.west)
+out = builder.create_output("out", N, Orientation.east)
 # xbar_i, xbar_o = [], []
 # xbar_i.extend(in_)
 for i, inst in enumerate(builder.instantiate(cluster, "cluster", N)):
-    builder.connect(clk,                inst.pins['clk'])
-    builder.connect(inst.pins["o"],     out[i])
+    builder.connect(clk, inst.pins["clk"])
+    builder.connect(inst.pins["o"], out[i])
 
 # each input pin of `inst` connects to 2 input ports of `clb`
 cons = [[] for _ in range(N)]
-for n, m in InterconnectAlgorithms.crossbar(len(in_), N, 2*k):
+for n, m in InterconnectAlgorithms.crossbar(len(in_), N, 2 * k):
     cons[m].append(n)
 print("iport-ipin:", cons)
 for i, con in enumerate(cons):
     for j, c in enumerate(con):
-        builder.connect(
-            in_[c], builder.instances["cluster", i].pins["i"][j % k])
+        builder.connect(in_[c], builder.instances["cluster", i].pins["i"][j % k])
 
 # each input pin of `inst` connects to 1 output pins of another `inst`
 cons = [[] for _ in range(N)]
@@ -71,8 +70,10 @@ for n, m in InterconnectAlgorithms.crossbar(N, N, k):
 print("opin-ipin:", cons)
 for i, con in enumerate(cons):
     for j, c in enumerate(con):
-        builder.connect(builder.instances["cluster", c].pins["o"],
-                        builder.instances["cluster", i].pins["i"][j % k])
+        builder.connect(
+            builder.instances["cluster", c].pins["o"],
+            builder.instances["cluster", i].pins["i"][j % k],
+        )
 
     # xbar_i.extend(inst.pins["o"])
     # xbar_o.extend(inst.pins["i"])
@@ -89,12 +90,16 @@ clbtile = ctx.build_tile(clb).fill((fs_i, fs_o)).auto_connect().commit()
 # IO tile
 iotiles = {}
 for ori in Orientation:
-    builder = ctx.build_tile(iob, n_io, name="t_io_{}".format(ori.name[0]),
-                             edge=OrientationTuple(False, **{ori.name: True}))
-    iotiles[ori] = builder.fill((.5, .5)).auto_connect().commit()
+    builder = ctx.build_tile(
+        iob,
+        n_io,
+        name="t_io_{}".format(ori.name[0]),
+        edge=OrientationTuple(False, **{ori.name: True}),
+    )
+    iotiles[ori] = builder.fill((0.5, 0.5)).auto_connect().commit()
 
 # Top
-builder = ctx.build_array('top', W, H, set_as_top=True)
+builder = ctx.build_array("top", W, H, set_as_top=True)
 for x, y in product(range(builder.width), range(builder.height)):
     if x in (0, builder.width - 1) and y in (0, builder.height - 1):
         pass
@@ -112,9 +117,9 @@ for x, y in product(range(builder.width), range(builder.height)):
 top = builder.fill(SwitchBoxPattern.cycle_free).auto_connect().commit()
 
 Flow(
-    VPRArchGeneration('vpr/arch.xml'),
-    VPR_RRG_Generation('vpr/rrg.xml'),
-    YosysScriptsCollection('syn'),
+    VPRArchGeneration("vpr/arch.xml"),
+    VPR_RRG_Generation("vpr/rrg.xml"),
+    YosysScriptsCollection("syn"),
 ).run(ctx)
 
 
@@ -140,11 +145,11 @@ def iter_instances(module):
 
 
 Flow(
-    Materialization('scanchain', chain_width=chain_width),
+    Materialization("scanchain", chain_width=chain_width),
     Translation(),
     SwitchPathAnnotation(),
     ProgCircuitryInsertion(iter_instances=iter_instances),
-    VerilogCollection('rtl'),
+    VerilogCollection("rtl"),
 ).run(ctx)
 
 # remove unusable I/O from IOPlanner (due to physical I/O limitations of the caravel wrapper)
@@ -157,7 +162,6 @@ def create_io(x, y, subtile, global_=None):
 ctx.summary.ios = [
     # hard-wired clock
     create_io(0, 1, 0, gbl_clk),
-
     # west bank: 10 I/Os
     create_io(0, 1, 1),
     create_io(0, 2, 0),
@@ -169,39 +173,40 @@ ctx.summary.ios = [
     create_io(0, 6, 1),
     create_io(0, 7, 0),
     create_io(0, 8, 0),
-
     # north bank: 10 I/Os
-    create_io(1, H-1, 0),
-    create_io(2, H-1, 0),
-    create_io(2, H-1, 1),
-    create_io(3, H-1, 0),
-    create_io(4, H-1, 0),
-    create_io(5, H-1, 0),
-    create_io(6, H-1, 0),
-    create_io(7, H-1, 0),
-    create_io(7, H-1, 1),
-    create_io(8, H-1, 1),
-
+    create_io(1, H - 1, 0),
+    create_io(2, H - 1, 0),
+    create_io(2, H - 1, 1),
+    create_io(3, H - 1, 0),
+    create_io(4, H - 1, 0),
+    create_io(5, H - 1, 0),
+    create_io(6, H - 1, 0),
+    create_io(7, H - 1, 0),
+    create_io(7, H - 1, 1),
+    create_io(8, H - 1, 1),
     # east bank: 12 I/Os, but less recommended for use
     # create_io (9, 8, 1),
-    create_io(W-1, 8, 0),
-    create_io(W-1, 7, 1),
-    create_io(W-1, 7, 0),
-    create_io(W-1, 6, 0),
-    create_io(W-1, 5, 1),
-    create_io(W-1, 5, 0),
-    create_io(W-1, 4, 0),
-    create_io(W-1, 3, 0),
-    create_io(W-1, 2, 1),
-    create_io(W-1, 2, 0),
+    create_io(W - 1, 8, 0),
+    create_io(W - 1, 7, 1),
+    create_io(W - 1, 7, 0),
+    create_io(W - 1, 6, 0),
+    create_io(W - 1, 5, 1),
+    create_io(W - 1, 5, 0),
+    create_io(W - 1, 4, 0),
+    create_io(W - 1, 3, 0),
+    create_io(W - 1, 2, 1),
+    create_io(W - 1, 2, 0),
     # create_io (9, 1, 1),
-    create_io(W-1, 1, 0),
+    create_io(W - 1, 1, 0),
 ]
 
 ctx.pickle("ctx.pkl" if len(sys.argv) < 2 else sys.argv[1])
 
-visited, next_, flist = [clbtile.key], [
-    ctx.database[ModuleView.design, clbtile.key]], []
+visited, next_, flist = (
+    [clbtile.key],
+    [ctx.database[ModuleView.design, clbtile.key]],
+    [],
+)
 while next_:
     d = next_.pop(0)
     flist.append(getattr(d, "verilog_src", d.name + ".v"))
@@ -209,14 +214,13 @@ while next_:
         if sub.model.key not in visited:
             next_.append(sub.model)
             visited.append(sub.model.key)
-with open("rtl/Flist.clb", 'w') as f:
+with open("rtl/Flist.clb", "w") as f:
     # f.write("rtl/include/prga_utils.vh\n")
     # f.write("rtl/include/pktchain.vh\n")
     for ff in flist:
         f.write("rtl/" + ff + "\n")
 
-visited, next_, flist = [top.key], [
-    ctx.database[ModuleView.design, top.key]], []
+visited, next_, flist = [top.key], [ctx.database[ModuleView.design, top.key]], []
 while next_:
     d = next_.pop(0)
     flist.append(getattr(d, "verilog_src", d.name + ".v"))
@@ -224,7 +228,7 @@ while next_:
         if sub.model.key not in visited and sub.model.key != clbtile.key:
             next_.append(sub.model)
             visited.append(sub.model.key)
-with open("rtl/Flist.top", 'w') as f:
+with open("rtl/Flist.top", "w") as f:
     # f.write("rtl/include/prga_utils.vh\n")
     # f.write("rtl/include/pktchain.vh\n")
     for ff in flist:
@@ -252,7 +256,6 @@ with open("pinout.txt", "w") as f:
     f.write(s)
 
     for io, id_ in zip(ctx.summary.ios[1:], reversed(range(2, 34))):
-        f.write(tmpl.format(pin=id_, x=io.position.x,
-                y=io.position.y, z=io.subtile))
+        f.write(tmpl.format(pin=id_, x=io.position.x, y=io.position.y, z=io.subtile))
 
     f.write("\n")
